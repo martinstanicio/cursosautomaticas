5c6383d406fae71ccaa6a6542d3cb712e4681d32
Martín Stanicio <martuytimo@gmail.com>
2023-07-29 02:34:02 -0300

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,57 @@
+import "./globals.css";
+
+import { Nunito, Play } from "next/font/google";
+
+import type { Metadata } from "next";
+
+const headings = Play({
+  variable: "--font-headings",
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
+const body = Nunito({ variable: "--font-body", subsets: ["latin"] });
+
+const siteName = "Cursos Automáticas";
+const description =
+  "Conduce la excelencia en tu aprendizaje con Cursos Automáticas";
+const themeColor = "#ef4444";
+
+// TODO add images to metadata
+// * https://nextjs.org/docs/app/building-your-application/optimizing/metadata#dynamic-image-generation
+// const images = "";
+
+// TODO add url to metadata
+// const url = "";
+
+export const metadata: Metadata = {
+  title: {
+    template: `%s | ${siteName}`,
+    default: siteName,
+  },
+  description,
+  themeColor,
+  colorScheme: "dark",
+  creator: "Martín Stanicio",
+  generator: "Next.js",
+  openGraph: {
+    siteName,
+    title: siteName,
+    description,
+    type: "website",
+    locale: "es_ES",
+    // url,
+    // images,
+  },
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="es" className={`${headings.variable} ${body.variable}`}>
+      <body className="font-body">{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,20 @@
+export default function Home() {
+  return (
+    <main className="container">
+      <h1 className="font-headings text-3xl font-bold text-neutral-50 sm:text-4xl md:text-5xl lg:text-6xl">
+        Conduce la excelencia en tu aprendizaje con{" "}
+        <span className="text-accent-600">Cursos Automáticas</span>
+      </h1>
+      <p className="text-neutral-300">
+        Aprende de la experiencia de Carlos Grosso, experto en reparación de
+        cajas automáticas.
+      </p>
+      <a
+        href="#"
+        className="inline-block bg-accent-600 px-7 py-4 font-bold uppercase text-neutral-50"
+      >
+        ¡Regístrate ahora!
+      </a>
+    </main>
+  );
+}
