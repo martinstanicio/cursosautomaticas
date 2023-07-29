fda04a4a8b2dfa061b2d0dd1cfdaa6d798e15dd8
Martín Stanicio <martuytimo@gmail.com>
2023-07-29 16:20:53 -0300

    fix class order

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Button from "@/components/Button";
 export default function Home() {
   return (
     <main className="container">
-      <h1 className="text-3xl font-bold font-headings text-neutral-50 sm:text-4xl md:text-5xl lg:text-6xl">
+      <h1 className="font-headings text-3xl font-bold text-neutral-50 sm:text-4xl md:text-5xl lg:text-6xl">
         Conduce la excelencia en tu aprendizaje con{" "}
         <span className="text-accent-600">Cursos Automáticas</span>
       </h1>
