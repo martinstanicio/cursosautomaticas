ecb63d80d51f4411304e3977b0cdd68cdd9765b3
Martín Stanicio <martuytimo@gmail.com>
2023-07-30 04:01:21 -0300

    fix header spacing on mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,7 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="flex items-center justify-between px-6 py-4">
+    <header className="flex items-center justify-between px-2 py-4 sm:px-4">
       <Brand textClassName="hidden min-[400px]:block" />
 
       <Menu as={"div"} className="relative">
