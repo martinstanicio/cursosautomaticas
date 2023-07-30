377bfbeeb80becb7fa8b9f7e186653b6b7784463
Martín Stanicio <martuytimo@gmail.com>
2023-07-30 03:55:35 -0300

    fix nav links accesibility on desktop

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,59 +7,6 @@ import Brand from "./Brand";
 import Button from "./Button";
 import Link from "next/link";
 
-export function Nav({
-  _static = false,
-  className,
-}: {
-  _static?: boolean;
-  className?: string;
-}) {
-  return (
-    <Menu.Items
-      static={_static}
-      as={"nav"}
-      className={`${className} flex gap-6 font-bold uppercase`}
-    >
-      <Menu.Item>
-        {({ active }) => (
-          <Link
-            href="/cursos"
-            className={`border-bottom-2 border-transparent ${
-              active && "border-neutral-50 text-neutral-50"
-            }`}
-          >
-            Cursos
-          </Link>
-        )}
-      </Menu.Item>
-      <Menu.Item>
-        {({ active }) => (
-          <Link
-            href="/login"
-            className={`border-bottom-2 border-transparent ${
-              active && "border-neutral-50 text-neutral-50"
-            }`}
-          >
-            Iniciar sesión
-          </Link>
-        )}
-      </Menu.Item>
-      <Menu.Item>
-        {({ active }) => (
-          <Button
-            href="/signup"
-            size="small"
-            intent="primary"
-            className={`${active && "bg-accent-700"}`}
-          >
-            ¡Regístrate ahora!
-          </Button>
-        )}
-      </Menu.Item>
-    </Menu.Items>
-  );
-}
-
 export default function Header() {
   return (
     <header className="flex items-center justify-between px-6 py-4">
@@ -90,12 +37,71 @@ export default function Header() {
                 leaveFrom="transform scale-100 opacity-100"
                 leaveTo="transform scale-95 opacity-0"
               >
-                <Nav className="absolute right-0 mt-2 w-64 origin-top-right flex-col bg-neutral-800 p-6 shadow before:absolute before:-top-2 before:right-2 before:h-0 before:w-0 before:border-x-8 before:border-b-8 before:border-x-transparent before:border-b-neutral-800" />
+                <Menu.Items
+                  static
+                  as={"nav"}
+                  className="absolute right-0 mt-2 flex w-64 origin-top-right flex-col gap-6 bg-neutral-800 p-6 font-bold uppercase shadow before:absolute before:-top-2 before:right-2 before:h-0 before:w-0 before:border-x-8 before:border-b-8 before:border-x-transparent before:border-b-neutral-800"
+                >
+                  <Menu.Item>
+                    {({ active }) => (
+                      <Link
+                        href="/cursos"
+                        className={`${active && "text-neutral-50"}`}
+                      >
+                        Cursos
+                      </Link>
+                    )}
+                  </Menu.Item>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <Link
+                        href="/login"
+                        className={`${active && "text-neutral-50"}`}
+                      >
+                        Iniciar sesión
+                      </Link>
+                    )}
+                  </Menu.Item>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <Button
+                        href="/signup"
+                        size="small"
+                        intent="primary"
+                        className={`${active && "bg-accent-700"}`}
+                      >
+                        ¡Regístrate ahora!
+                      </Button>
+                    )}
+                  </Menu.Item>
+                </Menu.Items>
               </Transition>
             </div>
-            <div className="hidden lg:block">
-              <Nav _static className="items-center" />
-            </div>
+            <nav className="hidden lg:block">
+              <ul className="flex items-center gap-6 font-bold uppercase">
+                <li>
+                  <Link
+                    href="/cursos"
+                    className="hover:text-neutral-50 focus:text-neutral-50"
+                  >
+                    Cursos
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/login"
+                    className="hover:text-neutral-50 focus:text-neutral-50"
+                  >
+                    Iniciar sesión
+                  </Link>
+                </li>
+                <li>
+                  <Button href="/signup" size="small" intent="primary">
+                    ¡Regístrate ahora!
+                  </Button>
+                </li>
+              </ul>
+            </nav>
           </>
         )}
       </Menu>
