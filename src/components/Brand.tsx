d926710cca56061df4d57fc1cb84c6e6a2636605
Martín Stanicio <martuytimo@gmail.com>
2023-07-30 02:28:59 -0300

    adjust Brand component sizing

--- src/components/Brand.tsx
+++ src/components/Brand.tsx
@@ -7,12 +7,12 @@ export interface Props {
 
 export default function Brand({ textClassName = "" }: Props) {
   return (
-    <Link href="/" className="flex items-center gap-3 sm:gap-6">
-      <div className="relative aspect-square w-12 sm:w-16">
+    <Link href="/" className="flex items-center gap-3 sm:gap-4 lg:gap-6">
+      <div className="relative aspect-square w-10 sm:w-12 lg:w-14">
         <Image src="/logo.png" alt="logo" fill />
       </div>
       <span
-        className={`font-headings text-2xl font-bold sm:text-4xl ${textClassName}`}
+        className={`font-headings text-2xl font-bold sm:text-3xl lg:text-4xl ${textClassName}`}
       >
         Cursos Automáticas
       </span>
