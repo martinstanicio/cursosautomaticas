f915cdd4531cc562266da48d1136c7f65551e241
Martín Stanicio <martuytimo@gmail.com>
2023-07-30 23:52:06 -0300

    fix Footer spacing

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <div className="bg-black">
-      <footer className="container mx-auto flex max-w-4xl flex-col gap-8 px-2 py-16 sm:px-4">
+      <footer className="container mx-auto flex max-w-4xl flex-col gap-12 px-2 py-16 sm:px-4">
         <Brand />
         <ul className="grid list-none grid-cols-1 gap-y-2 sm:grid-cols-2">
           <li>
