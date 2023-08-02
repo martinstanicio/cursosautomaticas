0f04bbda2fedb322891e7e0e44fbcab8997e50df
Martín Stanicio <martuytimo@gmail.com>
2023-08-02 20:26:08 -0300

    create about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,9 @@ import {
 } from "@carbon/icons-react";
 
 import Benefit from "@/components/Benefit";
+import Button from "@/components/Button";
 import Hero from "@/components/Hero";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -36,6 +38,37 @@ export default function Home() {
             del mundo, donde podrás hacer preguntas y recibir respuestas en
             tiempo real.
           </Benefit>
+        </div>
+      </section>
+      <section className="bg-black">
+        <div className="mx-auto grid max-w-5xl gap-12 px-2 py-16 sm:px-4 md:grid-cols-2 md:px-8 lg:px-12">
+          <div className="max-w-prose">
+            <h2 className="mb-8">
+              Conoce a Carlos Grosso, nuestro especialista
+            </h2>
+            <div className="space-y-4">
+              <p>
+                <strong>Carlos Grosso</strong> es un apasionado de las cajas
+                automáticas con más de{" "}
+                <strong className="text-accent-500">
+                  35 años de experiencia
+                </strong>{" "}
+                en el campo. Durante los últimos 6 años, ha sido profesor en la
+                prestigiosa <strong className="text-accent-500">FAATRA</strong>,
+                donde ha formado a nuevos profesionales del área.
+              </p>
+              <p>
+                Prepárate para aprender de un referente en la industria y{" "}
+                <strong>desbloquear nuevas oportunidades</strong> en tu carrera.
+              </p>
+              <Button href="#" size="small">
+                Leer más
+              </Button>
+            </div>
+          </div>
+          <div className="relative border-8 border-accent-500">
+            <Image src="/carlosgrosso.jpg" alt="" fill objectFit="cover" />
+          </div>
         </div>
       </section>
     </>
