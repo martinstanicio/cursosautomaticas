2261c19c1e3cf9fff41b73c431a84c9862efe6f5
Martín Stanicio <martuytimo@gmail.com>
2023-08-05 02:04:41 -0300

    configure contentlayer

---
+++ contentlayer.config.ts
@@ -0,0 +1,25 @@
+import { defineDocumentType, makeSource } from "contentlayer/source-files";
+
+import type { LocalDocument } from "contentlayer/source-files";
+
+export const Course = defineDocumentType(() => ({
+  name: "Course",
+  filePathPattern: `**/*.md`,
+  fields: {
+    title: { type: "string", required: true },
+    description: { type: "string", required: true },
+  },
+  computedFields: {
+    url: {
+      type: "string",
+      resolve: (course: LocalDocument) =>
+        `/cursos/${course._raw.flattenedPath}`,
+    },
+  },
+}));
+
+export default makeSource({
+  contentDirPath: "content",
+  documentTypes: [Course],
+  disableImportAliasWarning: true,
+});
