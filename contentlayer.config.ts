7ee17ca0246891734ce6205fc872af343fb20a8b
Martín Stanicio <martuytimo@gmail.com>
2023-08-19 15:46:19 -0300

    style sections in course pages

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,10 +1,12 @@
 import { defineDocumentType, makeSource } from "contentlayer/source-files";
 
 import type { LocalDocument } from "contentlayer/source-files";
+import sectionize from "@hbsnow/rehype-sectionize";
 
 export const Course = defineDocumentType(() => ({
   name: "Course",
-  filePathPattern: `**/*.md`,
+  filePathPattern: `**/*.(md|mdx)`,
+  contentType: "mdx",
   fields: {
     title: { type: "string", required: true },
     description: { type: "string", required: true },
@@ -23,4 +25,7 @@ export default makeSource({
   contentDirPath: "content",
   documentTypes: [Course],
   disableImportAliasWarning: true,
+  mdx: {
+    rehypePlugins: [sectionize],
+  },
 });
--- src/app/cursos/[slug]/page.tsx
+++ src/app/cursos/[slug]/page.tsx
@@ -1,7 +1,9 @@
 import Heading from "@/components/Heading";
 import Image from "next/image";
+import Link from "next/link";
 import { allCourses } from "contentlayer/generated";
 import { notFound } from "next/navigation";
+import { useMDXComponent } from "next-contentlayer/hooks";
 
 export const generateStaticParams = () =>
   allCourses.map((c) => ({ slug: c._raw.flattenedPath }));
@@ -15,12 +17,14 @@ export function generateMetadata({ params }: { params: { slug: string } }) {
 export default function Curso({ params }: { params: { slug: string } }) {
   const course = allCourses.find((c) => c._raw.flattenedPath === params.slug);
   if (!course) notFound();
+
+  const Content = useMDXComponent(course.body.code);
   const imgPath = `/${course._raw.flattenedPath}.jpg`;
 
   return (
-    <article className="mx-auto max-w-3xl space-y-8 px-2 py-16 marker:font-bold marker:text-accent-500 sm:px-4 md:px-8 lg:px-12">
-      <header>
-        <Heading as="h1" size={2}>
+    <article>
+      <header className="mx-auto max-w-3xl px-2 py-16 sm:px-4 md:px-8 lg:px-12">
+        <Heading as="h1" size={1}>
           {course.title}
         </Heading>
         <div className="relative aspect-[4/3]">
@@ -33,7 +37,60 @@ export default function Curso({ params }: { params: { slug: string } }) {
           />
         </div>
       </header>
-      <div dangerouslySetInnerHTML={{ __html: course.body.html }} />
+      <Content
+        components={{
+          // ! fix typescript error
+          section: ({
+            children,
+            ["data-heading-rank"]: headingRank,
+          }: React.HTMLAttributes<HTMLElement> & {
+            "data-heading-rank"?: string;
+          }) => {
+            switch (Number(headingRank)) {
+              case 2:
+                return (
+                  <section className="even:bg-black">
+                    <div className="mx-auto max-w-3xl px-2 py-16 sm:px-4 md:px-8 lg:px-12">
+                      {children}
+                    </div>
+                  </section>
+                );
+              case 3:
+                return (
+                  <section className="mt-8 first-of-type:mt-0">
+                    {children}
+                  </section>
+                );
+
+              default:
+                return children;
+            }
+          },
+          a: ({ href, children }) => (
+            <Link href={href as string}>{children}</Link>
+          ),
+          h2: ({ children }) => (
+            <Heading as="h2" size={2} className="pb-[1em]">
+              {children}
+            </Heading>
+          ),
+          h3: ({ children }) => (
+            <Heading as="h3" size={3} className="pb-[1em]">
+              {children}
+            </Heading>
+          ),
+          ol: ({ children }) => (
+            <ol className="list-decimal ps-6 marker:font-bold marker:text-accent-500">
+              {children}
+            </ol>
+          ),
+          ul: ({ children }) => (
+            <ul className="list-square ps-6 marker:text-accent-500">
+              {children}
+            </ul>
+          ),
+        }}
+      />
     </article>
   );
 }
