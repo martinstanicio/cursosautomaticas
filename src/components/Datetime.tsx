58079082e74056f42eb69f34785ccd0ab3c6571a
Martín Stanicio <martuytimo@gmail.com>
2023-09-06 23:23:29 -0300

    add className attr to Datetime component

--- src/components/Datetime.tsx
+++ src/components/Datetime.tsx
@@ -1,10 +1,23 @@
 import { Calendar, Time } from "@carbon/icons-react";
 
-export default function Datetime({ datetime }: { datetime: Date }) {
+import { twMerge } from "tailwind-merge";
+
+export default function Datetime({
+  datetime,
+  className,
+}: {
+  datetime: Date;
+  className?: string;
+}) {
   return (
-    <time className="grid w-full grid-cols-2 gap-4 text-sm text-neutral-50">
+    <time
+      className={twMerge(
+        "grid w-full grid-cols-2 gap-4 fill-accent-500 text-sm text-neutral-50",
+        className,
+      )}
+    >
       <div className="flex items-center gap-2">
-        <Calendar size={32} className="fill-accent-500" />
+        <Calendar size={32} className="fill-inherit" />
         {datetime.toLocaleDateString("es", {
           day: "2-digit",
           month: "2-digit",
@@ -12,7 +25,7 @@ export default function Datetime({ datetime }: { datetime: Date }) {
         })}
       </div>
       <div className="flex items-center gap-2">
-        <Time size={32} className="fill-accent-500" />
+        <Time size={32} className="fill-inherit" />
         {datetime.toLocaleTimeString("es", {
           hour: "2-digit",
           minute: "2-digit",
