b6a0626ff5a69150623dcdf544e397e8569d11e7
Martín Stanicio <martuytimo@gmail.com>
2023-09-09 17:32:00 -0300

    approved purchase functionality

--- src/components/Checkout.tsx
+++ src/components/Checkout.tsx
@@ -2,17 +2,19 @@
 
 import type { Course } from "contentlayer/generated";
 import { PayPalButtons } from "@paypal/react-paypal-js";
+import { useState } from "react";
 
 export default function Checkout({
   title,
   price,
 }: Pick<Course, "title" | "price">) {
-  return (
+  const [isPurchased, setIsPurchased] = useState(false);
+
+  const CheckoutButtons = (
     <PayPalButtons
-      className="min-h-[13rem] rounded bg-white p-4"
       style={{ color: "black" }}
-      createOrder={(data, actions) =>
-        actions.order.create({
+      createOrder={(data, actions) => {
+        return actions.order.create({
           purchase_units: [
             {
               amount: { value: price.toString() },
@@ -20,8 +22,30 @@ export default function Checkout({
             },
           ],
           application_context: { shipping_preference: "NO_SHIPPING" },
-        })
-      }
+        });
+      }}
+      onApprove={async (data, actions) => {
+        const order = await actions.order?.capture();
+        if (order?.status === "COMPLETED") {
+          setIsPurchased(true);
+        }
+      }}
     />
+  );
+
+  const CheckoutSuccess = (
+    <>
+      <p>Su compra ha sido realizada con éxito.</p>
+      <p>
+        Nos comunicaremos con usted mediante email para informarle sobre el
+        curso en la brevedad.
+      </p>
+    </>
+  );
+
+  return (
+    <div className="min-h-[13rem] rounded bg-white p-4 text-neutral-900">
+      {!isPurchased ? CheckoutButtons : CheckoutSuccess}
+    </div>
   );
 }
