9e3530fbe9220159f5185ca5d71789de61c959d6
Martín Stanicio <martuytimo@gmail.com>
2023-10-11 15:42:15 -0300

    fix blurry logo

--- src/components/Brand.tsx
+++ src/components/Brand.tsx
@@ -11,7 +11,7 @@ export default function Brand({ textClassName }: Props) {
   return (
     <Link href="/" className="flex items-center gap-3 sm:gap-4 lg:gap-6">
       <div className="relative aspect-square w-10 sm:w-12 lg:w-14">
-        <Image sizes="56px" src="/logo.png" alt="logo" fill />
+        <Image src="/logo.png" alt="logo" fill />
       </div>
       <span
         className={twMerge(
