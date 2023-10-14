bb8f43d7740a0f18b3d2249517f6ce4f6eacd7d6
Martín Stanicio <martuytimo@gmail.com>
2023-10-13 21:38:19 -0300

    refactor PayPalProvider for readability

--- src/providers/PayPal.tsx
+++ src/providers/PayPal.tsx
@@ -4,15 +4,15 @@ import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 
 import { getEnv } from "@/lib/env";
 
-export default function PayPalProvider({
-  children,
-}: {
+export interface Props {
   children: React.ReactNode;
-}) {
+}
+
+export default function PayPalProvider({ children }: Props) {
+  const NEXT_PUBLIC_PAYPAL_CLIENT_ID = getEnv("NEXT_PUBLIC_PAYPAL_CLIENT_ID");
+
   return (
-    <PayPalScriptProvider
-      options={{ clientId: getEnv("NEXT_PUBLIC_PAYPAL_CLIENT_ID") }}
-    >
+    <PayPalScriptProvider options={{ clientId: NEXT_PUBLIC_PAYPAL_CLIENT_ID }}>
       {children}
     </PayPalScriptProvider>
   );
